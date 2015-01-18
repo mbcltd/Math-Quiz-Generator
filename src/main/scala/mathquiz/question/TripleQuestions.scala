11ace1f3988fce1d92c1bf66e60711f42a39b804
David Morgan-Brown <david@morgan-brown.com>
2015-01-18 13:05:02 +0000

    Removed a bunch of duplication, added QBC type questions.

--- src/main/scala/mathquiz/question/TripleQuestions.scala
+++ src/main/scala/mathquiz/question/TripleQuestions.scala
@@ -4,65 +4,91 @@ import mathquiz.Question
 
 import scala.util.Random
 
+
 object TripleQuestions {
   val all = ABQMultipleQuestion ::
     ABQAddQuestion ::
     ABQDivideQuestion ::
     AQBMultipleQuestion ::
     AQBAddQuestion ::
-    AQBDivideQuestion :: Nil
+    AQBDivideQuestion ::
+    QBCMultipleQuestion ::
+    QBCAddQuestion ::
+    QBCDivideQuestion :: Nil
 }
 
-object ABQMultipleQuestion extends Question {
-  def text = {
+case class TripleEquation(a:Int, b:Int, c:Int)
+
+trait Multiply {
+  def generate:TripleEquation = {
     val a = Random.nextInt(10)
     val b = Random.nextInt(10)
-    ""+a+" × "+b + " = ?"
+    TripleEquation(a,b,a*b)
   }
+  val symbol = "×"
 }
 
-object ABQAddQuestion extends Question {
-  def text = {
+trait Add {
+  def generate:TripleEquation = {
     val a = Random.nextInt(10)
     val b = Random.nextInt(10)
-    ""+a+" + "+b + " = ?"
+    TripleEquation(a,b,a+b)
   }
+  val symbol = "+"
 }
 
-object ABQDivideQuestion extends Question {
-  def text = {
+
+trait Divide {
+  def generate:TripleEquation = {
     val b = Random.nextInt(9)+1
-    val t = Random.nextInt(10)
-    val a = b * t
-    ""+a+" ÷ "+b + " = ?"
+    val c = Random.nextInt(10)
+    TripleEquation(b*c,b,c)
   }
+  val symbol = "÷"
 }
 
-object AQBMultipleQuestion extends Question {
+trait ABTripleQuestion extends Question {
+  def generate:TripleEquation
+  val symbol:String
   def text = {
-    val a = Random.nextInt(10)
-    val b = Random.nextInt(10)
-    val q = a * b
-    ""+a+" × ? = "+q
+    val t = generate
+    s"${t.a} $symbol ${t.b} = ?"
   }
 }
 
-
-object AQBAddQuestion extends Question {
+trait ACTripleQuestion extends Question {
+  def generate:TripleEquation
+  val symbol:String
   def text = {
-    val a = Random.nextInt(10)
-    val b = Random.nextInt(10)
-    val q = a + b
-    ""+a+" + ? = "+q
+    val t = generate
+    s"${t.a} $symbol ? = ${t.c}"
   }
 }
 
 
-object AQBDivideQuestion extends Question {
+trait BCTripleQuestion extends Question {
+  def generate:TripleEquation
+  val symbol:String
   def text = {
-    val b = Random.nextInt(9) + 1
-    val q = Random.nextInt(10)
-    val a = b * q
-    ""+a+" + ? = "+q
+    val t = generate
+    s"? $symbol ${t.b} = ${t.c}"
   }
 }
+
+object ABQMultipleQuestion extends ABTripleQuestion with Multiply
+
+object ABQAddQuestion extends ABTripleQuestion with Add
+
+object ABQDivideQuestion extends ABTripleQuestion with Divide
+
+object AQBMultipleQuestion extends ACTripleQuestion with Multiply
+
+object AQBAddQuestion extends ACTripleQuestion with Add
+
+object AQBDivideQuestion extends ACTripleQuestion with Divide
+
+object QBCMultipleQuestion extends BCTripleQuestion with Multiply
+
+object QBCAddQuestion extends BCTripleQuestion with Add
+
+object QBCDivideQuestion extends BCTripleQuestion with Divide
